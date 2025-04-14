b87339653ecf645047e1a814397e6fd2aca4a8f7
RDSMatheus <matheus_rds1998@hotmail.com>
2025-04-14 00:38:31 -0300

    fix bug nas variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,13 +18,15 @@ import { GlobalHttpModule } from './http.module';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
-        host: 'localhost',
+        host: process.env.DB_HOST,
         port: 5432,
         username: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
+        ssl: true,
+        autoLoadEntities: true,
+        synchronize: false,
         entities: [Pokemon],
-        synchronize: true,
       }),
     }),
     PokemonModule,
